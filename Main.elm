3fbd7e203406d055da4da0cbad54c048633cc7a9
Jesse Warden <jesterxl@jessewarden.com>
2018-07-07 11:02:04 -0400

    got random face working; should probably stick the date in teh random face as a 1 - 6 int

--- Main.elm
+++ Main.elm
@@ -1,5 +1,6 @@
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Random
 
 -- MODEL
  
@@ -15,12 +16,14 @@ type alias DieRoll = {
 
 type alias Model = {
   seed: Int,
+  dieFace: Int,
   dieRollResult: DieRollResult
 }
 
 model : Model
 model = {
   seed = 0,
+  dieFace = 1,
   dieRollResult = DieRollResult 1 6 2 }
 
 type alias Flags =
@@ -28,26 +31,28 @@ type alias Flags =
   }
 
 init : Flags -> (Model, Cmd Msg )
-init flags = ( (Model flags.startTime (DieRollResult 1 6 2)), Cmd.none)
+init flags = (Model flags.startTime 1 (DieRollResult 1 6 3), Cmd.none)
 
 -- UPDATE
-
-type Msg = Seed Int | Roll DieRollResult
+type Msg = Seed Int | Roll | NewFace Int
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Seed seedValue ->
       ({ model | seed = seedValue }, Cmd.none)
-    Roll dieRollResult ->
-      ({ model | dieRollResult = dieRollResult }, Cmd.none)
+    NewFace newFace ->
+      ({model | dieFace = newFace}, Cmd.none)
+    Roll ->
+      ( model, Random.generate NewFace (Random.int 1 6))
+      
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick (Roll (DieRollResult 1 6 4) ) ] [ text "roll" ]
+    [ button [ onClick Roll ] [ text "roll" ]
     , div [] [ text (toString model) ]
     ]
 main: Program Flags Model Msg
