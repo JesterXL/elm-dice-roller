1c955e5869ab3ddc19606b19d7da6cb293e683cb
Jesse Warden <jesterxl@jessewarden.com>
2018-07-06 21:50:08 -0400

    elm-reactor is whack... ok, have a random seed now

--- Main.elm
+++ Main.elm
@@ -1,9 +1,6 @@
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
 -- MODEL
  
 type alias DieRollResult = {
@@ -16,26 +13,47 @@ type alias DieRoll = {
     whatType: Int
 }
 
-type alias Model = DieRollResult
+type alias Model = {
+  seed: Int,
+  dieRollResult: DieRollResult
+}
 
 model : Model
-model = DieRollResult 1 6 2
+model = {
+  seed = 0,
+  dieRollResult = DieRollResult 1 6 2 }
+
+type alias Flags =
+  { startTime : Int
+  }
+
+init : Flags -> (Model, Cmd Msg )
+init flags = ( (Model flags.startTime (DieRollResult 1 6 2)), Cmd.none)
 
 -- UPDATE
 
-type Msg = Roll
+type Msg = Seed Int | Roll DieRollResult
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Roll ->
-      DieRollResult 1 6 4
+    Seed seedValue ->
+      ({ model | seed = seedValue }, Cmd.none)
+    Roll dieRollResult ->
+      ({ model | dieRollResult = dieRollResult }, Cmd.none)
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Roll ] [ text "roll" ]
+    [ button [ onClick (Roll (DieRollResult 1 6 4) ) ] [ text "roll" ]
     , div [] [ text (toString model) ]
     ]
+main: Program Flags Model Msg
+main =
+  Html.programWithFlags
+  { init = init,
+    view = view,
+    update = update,
+    subscriptions = \_ -> Sub.none }
